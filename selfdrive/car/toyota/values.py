3d1e933b0ab0ec02cc1ca8b90db625a5a938bcf5
mxcoldhit <jsboomershine@gmail.com>
2023-03-17 14:14:07 -0500

    Update values.py

--- selfdrive/car/toyota/values.py
+++ selfdrive/car/toyota/values.py
@@ -1588,6 +1588,7 @@ FW_VERSIONS = {
     ],
     (Ecu.engine, 0x700, None): [
       b'\x01896634AE1001\x00\x00\x00\x00',
+      b'\x01896634A89100\x00\x00\x00\x00',
     ],
     (Ecu.fwdRadar, 0x750, 0xf): [
       b'\x018821F0R03100\x00\x00\x00\x00',
